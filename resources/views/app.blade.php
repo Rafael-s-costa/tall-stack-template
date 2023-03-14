935c6e5a4de2c01b2c68f0d2754c5a2b559bf0aa
Rafael-s-costa <rafael.s.costa96@gmail.com>
2023-03-14 15:38:29 +0000

    TallStack template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @vite('resources/css/app.css')
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <style>
+            
+        </style>
+    </head>
+    <body class="antialiased">
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('content')
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100">
+        <h1 class="text-3xl font-bold underline selection:text-black text-red-300">
+            Hello world!
+        </h1>
+    </div>
+@endsection
